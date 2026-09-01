48cfa18bd7bc4e2d34fa9ec6b64f7eb79c06646d
kiskaadee <fcortesbio@gmail.com>
2026-09-01 11:01:18 -0500

    feat(client): implement gRPC test client for Greeter and Time services

---
+++ src/pygrpc/client.py
@@ -0,0 +1,33 @@
+import logging
+
+import grpc
+
+from pygrpc.greeter.v1 import greeter_pb2, greeter_pb2_grpc
+
+
+def run() -> None:
+    # Open a gRPC channel to the running server
+    with grpc.insecure_channel("localhost:50051") as channel:
+
+        # Test the GreeterService
+        greeter_stub = greeter_pb2_grpc.GreeterServiceStub(channel)
+        hello_request = greeter_pb2.SayHelloRequest(name="Developer")
+        hello_response = greeter_stub.SayHello(hello_request)
+
+        print("GreeterService Response:")
+        print("Message: ", hello_response.message)
+
+        # Test the TimeService
+        time_stub = greeter_pb2_grpc.TimeServiceStub(channel)
+        time_request = greeter_pb2.TimeRequest(name="Developer")
+        time_response = time_stub.Time(time_request)
+
+        dt = time_response.current_time.ToDatetime()
+
+        print("TimeService Response")
+        print(f"Message: {time_response.message}")
+        print(f"Server Time: {dt.isoformat()} UTC.")
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    run()
